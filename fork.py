1de0d39d9b674292a8a93453ca5a7662bc674bbd
Rhys Ulerich <rhys.ulerich@gmail.com>
2019-08-11 08:51:20 -0400

    Explicit Future

--- fork.py
+++ fork.py
@@ -1,38 +1,92 @@
-import atexit
 import contextlib
+import enum
+import itertools
 import multiprocessing
 import os
-import signal
+import queue
+import sys
 
 class Jobserver:
-    """
-    A GNU Make-ish jobserver using a POSIX pipe to track available job slots.
-
-    All forked children will inherit the same collection of available slots.
-    See https://www.gnu.org/software/make/manual/html_node/POSIX-Jobserver.html
-    """
     def __init__(self, context, slots: int):
+        # Prepare required resources
         assert slots >= 0
-        self.r, self.w = context.Pipe()  # Cannot be garbage collected
-        os.write(self.w.fileno(), slots * b'x')  # Mark all slots available
+        self.context = context
+        self.slots = self.context.Queue(maxsize=slots)
 
-    @contextlib.contextmanager
-    def slot(self) -> None:
-        rfd, wfd = self.r.fileno(), self.w.fileno()
-        bs = []
-        while not bs:
-            bs.extend(bytes((b,)) for b in os.read(rfd, 1))
-        def cleanup():
-            while bs:
-                os.write(wfd, bs.pop())
-        atexit.register(cleanup)
+        # Issue one token for each requested slot
+        for i in range(slots):
+            self.slots.put(block=False)
+
+    def submit(self, fn, *args, **kwargs) -> 'Jobserver.Future':
+        # Consume one slot prior to acquiring any resources
+        token = self.slots.get(block=True)
         try:
-            yield
+            # Prepare required resources
+            queue = self.context.Queue()
+            args += (slots, token, queue, fn)
+            process = self.context.Process(target=Jobserver._run,
+                                           args=args,
+                                           kwargs=kwargs,
+                                           daemon=False)
+            # A process that successfully starts now responsible for token
+            process.start()
+        except:
+            self.slots.put(token, block=True)
+            raise
+
+        # Wrap up necessary details for the caller
+        return self.Future(process, queue)
+
+    class Future:
+        def __init__(self, process, queue):
+            assert self.process is not None
+            assert self.queue is not None
+            self.process = process
+            self.queue = queue
+            self.result = None
+
+        def done() -> bool:
+            if self.queue is not None:
+                try:
+                    self.result = self.queue.get(block=False)
+                    self.queue = None
+                    self.process.join()
+                    self.process = None
+                except queue.Empty:
+                    return False
+
+            return True
+
+        def result(block=True, timeout=None):
+            # May throw queue.Empty when non-blocking requested
+            if self.queue is not None:
+                self.result = self.queue.get(block=block, timeout=timeout)
+                self.queue = None
+                self.process.join()
+                self.process = None
+
+            # Raise any exception that occurred
+            if isinstance(self.result, Exception):
+                raise self.result
+
+            # Return
+            return result
+
+    # TODO preexec_hook?
+    @staticmethod
+    def _run(slots, token, queue, fn, *args, **kwargs) -> None:
+        try:
+            result = fn(*args, **kwargs)
+            queue.put(result, block=True)
+        except queue.Full as e:
+            raise RuntimeError('Logic error') from e
+        except Exception as e:
+            queue.put(e)
         finally:
-            cleanup()
-            atexit.unregister(cleanup)
+            slots.put(token, block=True)
 
 
+# FIXME START HERE
 def foo(js):
     with js.slot():
         print('Hello!')
